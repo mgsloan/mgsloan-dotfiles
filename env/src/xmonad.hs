662834b8508af20ae38f99286ad158eb2e77f1de
Michael Sloan <mgsloan@gmail.com>
2020-05-09 15:20:54 -0600

    Better shortcut for "what's this track?"

--- env/src/xmonad.hs
+++ env/src/xmonad.hs
@@ -248,7 +248,7 @@ keymap env =
   , ("M-S-<Up>", spotifySetVolume 100)
   , ("M-S-<Down>", spotifySetVolume 0)
   , ("M-m M-d", spotifyDebugPlayerInfo)
-  , ("M-m M-c", forkXio spotifyNotifyTrack)
+  , ("M-S-/", forkXio spotifyNotifyTrack)
 
   -- Brightness control
   , ("M-S-=", spawn (_envHomeDir env </> "env/scripts/brightness-increase.sh") ["30"])

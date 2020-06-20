9507fb332bd833414c235ad34b9e7d9c0a6772a2
Michael Sloan <mgsloan@gmail.com>
2020-06-20 12:26:32 -0600

    Fix scrot script handling of non HIDPI environment

--- env/scripts/scrot.sh
+++ env/scripts/scrot.sh
@@ -1,6 +1,6 @@
 #1/bin/bash -e
 
-if [ -n HIDPI ]; then
+if [ -n $HIDPI ]; then
     scrot ~/pics/screenshots-large/%F_%T.png --select --exec 'bash -c "convert $f -resize 50% ~/pics/screenshots/$n; google-chrome ~/pics/screenshots/$n"'
 else
     scrot ~/pics/screenshots/%F_%T.png --select --exec 'google-chrome $f'

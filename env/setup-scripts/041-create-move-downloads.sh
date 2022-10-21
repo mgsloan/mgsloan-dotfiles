0bce8f1f46913cd382051b108f71abd2e9d66c6b
Michael Sloan <mgsloan@gmail.com>
2022-10-20 20:53:39 -0600

    Improve move-downloads cron to only move items older than an hour

--- env/setup-scripts/041-create-move-downloads.sh
+++ env/setup-scripts/041-create-move-downloads.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -ev
+#!/bin/bash -e
 
-[ "$UID" -eq 0 ] || exec sudo USER_HOME="$HOME" bash -ex "$0" "$@"
+[ "$UID" -eq 0 ] || exec sudo USER_HOME="$HOME" bash -e "$0" "$@"
 
 TEMPLATE="$USER_HOME/env/templates/move-downloads.template"
-DESTINATION="/etc/cron.daily/move-downloads"
+DESTINATION="/etc/cron.hourly/move-downloads"
 
 envsubst < $TEMPLATE > $DESTINATION
+chmod ugo+x $DESTINATION
 
 echo ""
 echo "Contents of $DESTINATION is now:"

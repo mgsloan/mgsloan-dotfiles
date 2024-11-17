12d6c0693239179a343991a17c84c39d9d155174
mgsloan@gmail.com <mgsloan@gmail.com>
2024-11-16 17:36:55 -0800

    Update scripts for setting up battery notifications

--- env/setup-scripts/030-freshen.sh
+++ env/setup-scripts/030-freshen.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -ex
 
 "$HOME/.local/bin/freshen"
+
+gem install mustache
--- env/setup-scripts/040-udev-rules.sh
+++ env/setup-scripts/040-udev-rules.sh
@@ -2,5 +2,6 @@
 
 [ "$UID" -eq 0 ] || exec sudo USER_HOME="$HOME" USER_NAME="$LOGNAME" bash -e "$0" "$@"
 
-cp "$HOME/env/udev-rules/90-backlight.rules" /etc/udev/rules.d/
+../udev-rules/generate.sh
+../udev-rules/apply.sh
 usermod -a -G video "$USER_NAME"

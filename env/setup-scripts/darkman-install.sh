680078c004900fb71f714a04cc5a3db1e7f387f6
Michael Sloan <mgsloan@gmail.com>
2024-12-30 21:18:32 -0700

    Setup script improvements from doing new computer setup

--- env/setup-scripts/darkman-install.sh
+++ env/setup-scripts/darkman-install.sh
@@ -2,8 +2,5 @@
 
 [ "$UID" -eq 0 ] || exec sudo USER_HOME="$HOME" USER_NAME="$LOGNAME" bash -e "$0" "$@"
 
-gem install mustache
-
-../udev-rules/generate.sh
-../udev-rules/apply.sh
-usermod -a -G video "$USER_NAME"
+make install PREFIX=/usr
+systemctl --user enable --now darkman.service

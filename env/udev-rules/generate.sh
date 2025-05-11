043c89843fa60a2d99671969163b8028ef8d02fe
Michael Sloan <mgsloan@gmail.com>
2025-05-11 14:19:40 +0200

    Fix username for battery notifications

--- env/udev-rules/generate.sh
+++ env/udev-rules/generate.sh
@@ -1,14 +1,19 @@
 #!/bin/bash -ex
 
 # Note that this script requires 'mustache'
-# (http://mustache.github.io/).  I installed it via:
+# (http://mustache.github.io/).
 #
 # > gem install mustache
+
+if [ -z "$USER_NAME" ]; then
+    echo "Expected USER_NAME to be set."
+    exit 1
+fi
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
 
-echo "user: \"$USER\"" > env-vars.yaml
+echo "user: \"$USER_NAME\"" > env-vars.yaml
 mustache env-vars.yaml 99-batify.rules.mustache > 99-batify.rules
 echo "Successfully generated 99-batify.rules"

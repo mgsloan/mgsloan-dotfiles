e8c90c31b165d7b5356687ca167bce8d42aaeae6
Michael Sloan <mgsloan@gmail.com>
2025-06-27 15:57:46 -0600

    Sign git commits with ssh key

--- env/setup-scripts/011-git-configure.sh
+++ env/setup-scripts/011-git-configure.sh
@@ -3,3 +3,8 @@
 git config --global user.name "Michael Sloan"
 git config --global user.email "mgsloan@gmail.com"
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
+# Use ssh key for signing commits
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/id_ed25519.pub
+git config --global commit.gpgsign true

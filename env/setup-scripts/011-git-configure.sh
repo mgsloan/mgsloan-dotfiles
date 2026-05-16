b28ca576d5460129e56ed8cea3890737b0c726e4
Michael Sloan <mgsloan@gmail.com>
2026-05-16 16:00:00 -0600

    Set hub cli protocol to https

--- env/setup-scripts/011-git-configure.sh
+++ env/setup-scripts/011-git-configure.sh
@@ -3,6 +3,7 @@
 git config --global user.name "Michael Sloan"
 git config --global user.email "mgsloan@gmail.com"
 git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+git config --global hub.protocol ssh
 
 # Use ssh key for signing commits
 git config --global gpg.format ssh

de1d53d974a44b1b8b0dfd0751f89e3d46fe6e48
Michael Sloan <mgsloan@gmail.com>
2026-06-10 23:36:03 -0600

    Update notify-hourly.sh messages

--- env/scripts/notify-hourly.sh
+++ env/scripts/notify-hourly.sh
@@ -9,15 +9,15 @@ case "$CURRENT_HOUR" in
 
   "21")notify-send "time = 21:00" "Time to think about winding down";;
   "22")notify-send "time = 22:00" "Initiate wind down sequence!! :D";;
-  "23")notify-send "time = 23:00" "Hey! Stop ignoring this! Time to wind down!";;
-  "00")notify-send "MIDNIGHT" "You really should wind down. Tomorrow going well is the priority now.";;
+  "23")notify-send "time = 23:00" "Hey! Stop ignoring this! Time to wind down! What's so important?";;
+  "00")notify-send "MIDNIGHT" "You really should wind down. Tomorrow going well is the priority now.  Whatever you are working on can wait.";;
   "01")notify-send "THE TIME IS NOW ONE AM" "Don't be a degenerate!";;
-  "02")notify-send "THE TIME IS NOW TWO AM" "Seriously??";;
-  "03")notify-send "THE TIME IS NOW THREE AM" "You better have a darn good reason..";;
+  "02")notify-send "THE TIME IS NOW TWO AM" "Seriously?? You're fucking up!";;
+  "03")notify-send "THE TIME IS NOW THREE AM" "You better have a damn good reason..";;
 
   "07")notify-send "7 am - someone's up early, awesome!" "Good time to do something you've been procrastinating.";;
   "08")notify-send "8 am. Good morning!" "What can you do now to help make this a great day?";;
-  "10")notify-send "10 am. Coffee time!";;
+  "10")notify-send "10 am. Tea time!";;
 
-  *)notify-send "hour = $CURRENT_HOUR";;
+  *)notify-send "time is $CURRENT_HOUR:00";;
 esac
